1fd1146e21c5d27dd7a6572ed93c465bd6f7f05f
William062004 <89233287+William062004@users.noreply.github.com>
2024-06-04 09:29:58 -0500

    Update Program.cs

--- 1. API/Program.cs
+++ 1. API/Program.cs
@@ -44,7 +44,7 @@ builder.Services.AddScoped<IScheduleData, ScheduleSQLData>();
 builder.Services.AddScoped<IScheduleDomain, ScheduleDomain>();
 
 //Pomelo MySql Connection
-var connectionString = builder.Configuration.GetConnectionString("sql10709845");
+var connectionString = builder.Configuration.GetConnectionString("sql10711720");
 builder.Services.AddDbContext<StudyMentorDB>(
     dbContextOptions =>
     {

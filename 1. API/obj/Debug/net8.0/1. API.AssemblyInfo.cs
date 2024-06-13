02efdf5c1ed835f663e1dcf998a0ae6ea0077a5e
William062004 <U202117904@upc.edu.pe>
2024-06-13 03:48:20 -0500

    sdk 8.0

--- 1. API/obj/Debug/net8.0/1. API.AssemblyInfo.cs
+++ 1. API/obj/Debug/net8.0/1. API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1. API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ee9e547dbc0fcaaeb2bcd27a83c2b3d56f551e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac2815aab85a02f2ca8941e9d1ed1944fd90829f")]
 [assembly: System.Reflection.AssemblyProductAttribute("1. API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1. API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
